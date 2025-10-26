3c282e632afe82ea8038baa5de2fd319b65359d5
irfan-official <irfanofficial.work@gmail.com>
2025-10-26 22:12:01 +0600

    first commit

---
+++ src/routes/router.jsx
@@ -0,0 +1,57 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../pages/Home.jsx";
+import Page404 from "../pages/Page404.jsx";
+import Login from "../pages/Login.jsx";
+import Register from "../pages/Register.jsx";
+import Services from "../pages/Services.jsx";
+import ServicesDetails from "../pages/ServicesDetails.jsx";
+import ProtectedRoute from "../components/ProtectedRoute.jsx";
+import MyProfile from "../pages/MyProfile.jsx";
+import ForgotPassword from "../pages/ForgotPassword.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+  },
+  {
+    path: "/profile",
+    element: (
+      <ProtectedRoute>
+        <MyProfile />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/services",
+    Component: Services,
+    children: [
+      {
+        path: ":id",
+        element: (
+          <ProtectedRoute>
+            <ServicesDetails />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/register",
+    Component: Register,
+  },
+  {
+    path: "/reset-password",
+    Component: ForgotPassword,
+  },
+  {
+    path: "*",
+    Component: Page404,
+  },
+]);
+
+export default router;
